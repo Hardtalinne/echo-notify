f17ff5ea745d3d04f2f2b35da4a4fdc2f036349f
Aline Hardt Silveira <alinne.1904@gmail.com>
2026-02-27 13:10:02 -0300

    fix(devops): estabiliza kafka cp-kafka e build docker com jdk11

--- docker/kafka/init-topics.sh
+++ docker/kafka/init-topics.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
 set -e
 
+
 echo "Waiting kafka..."
-until /opt/bitnami/kafka/bin/kafka-topics.sh --bootstrap-server kafka:9092 --list >/dev/null 2>&1; do
+until kafka-topics --bootstrap-server kafka:9092 --list >/dev/null 2>&1; do
   sleep 2
 done
 
-/opt/bitnami/kafka/bin/kafka-topics.sh --create --if-not-exists --bootstrap-server kafka:9092 --topic echo.notify.send --partitions 3 --replication-factor 1
-/opt/bitnami/kafka/bin/kafka-topics.sh --create --if-not-exists --bootstrap-server kafka:9092 --topic echo.notify.retry --partitions 3 --replication-factor 1
-/opt/bitnami/kafka/bin/kafka-topics.sh --create --if-not-exists --bootstrap-server kafka:9092 --topic echo.notify.dlq --partitions 3 --replication-factor 1
+kafka-topics --create --if-not-exists --bootstrap-server kafka:9092 --topic echo.notify.send --partitions 3 --replication-factor 1
+kafka-topics --create --if-not-exists --bootstrap-server kafka:9092 --topic echo.notify.retry --partitions 3 --replication-factor 1
+kafka-topics --create --if-not-exists --bootstrap-server kafka:9092 --topic echo.notify.dlq --partitions 3 --replication-factor 1
 
 echo "Topics initialized"
